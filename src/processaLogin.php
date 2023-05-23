acbaf590922779747ac7732244566b2a5ac7122f
Flavio Francisco <flavyobass151@gmail.com>
2023-05-22 22:16:55 -0300

    iniciando validação de login

--- src/processaLogin.php
+++ src/processaLogin.php
@@ -1,2 +1,25 @@
 <?php
+session_start();
+
+$login = $_POST['login'];
+$senha = $_POST['senha'];
+
+//busca no banco por usuarioa.
+
+$result = mysql_query("SELECT * FROM `USUARIO`
+WHERE `NOME` = '$login' AND `SENHA`= '$senha'");
+
+if(mysql_num_rows ($result) > 0 )
+{
+$_SESSION['login'] = $login;
+$_SESSION['senha'] = $senha;
+header('location:site.php');
+}
+else{
+  unset ($_SESSION['login']);
+  unset ($_SESSION['senha']);
+  header('location:index.php');
+
+  }
+
 ?>
